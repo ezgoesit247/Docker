d5c45e7e85d196f7fcae94c0cb486d046b7be824
Brandon R. India <ezgoesit247@gmail.com>
2020-12-19 13:44:59 -0600

    Configured in full support for Centos-7 running systemd

--- c7-systemd/Dockerfile.sh
+++ c7-systemd/Dockerfile.sh
@@ -1,2 +1,10 @@
 FROM local/c7-systemd
-CMD ["/bin/bash"]
+CMD ["/sbin/init"]
+
+### RUN CONTAINER WITH THIS COMMAND:
+#  docker run --privileged -d -v /sys/fs/cgroup:/sys/fs/cgroup:ro --rm c7-systemd
+# THEN CONNECT WITH THIS COMMAND:
+#  docker exec -it <GUID_RETURNED_ABOVE> sh
+#
+### WRAPPED INTO A ONE-LINER:
+#  docker exec -it `docker run --privileged -d -v /sys/fs/cgroup:/sys/fs/cgroup:ro --name systemd c7-systemd` /bin/bash
