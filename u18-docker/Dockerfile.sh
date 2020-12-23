3765365e325656b6736db293280f4673931d9074
Brandon R. India <ezgoesit247@gmail.com>
2020-12-23 09:34:05 -0600

    renamed with u18- prefix

---
+++ u18-docker/Dockerfile.sh
@@ -0,0 +1,36 @@
+FROM local/u18-seed
+
+### DOCKER ###
+RUN sudo apt-get install -qq \
+      apt-transport-https \
+      ca-certificates \
+      gnupg-agent \
+      software-properties-common \
+   && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - \
+   && sudo apt-key fingerprint 0EBFCD88 \
+   && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
+   && sudo apt-get update \
+   && sudo apt-get install -qq \
+      docker-ce \
+      docker-ce-cli \
+      containerd.io
+RUN    echo '### DOCKER ###\n \
+if ! sudo service docker status; then sudo service docker start; fi && sleep 2 && sudo service docker status\n \
+cyan "Docker:"; docker --version\n \
+if sudo docker run --rm hello-world 2> /dev/null | grep -o "Hello from Docker!"\n \
+  then pass "Docker Hello World"\n \
+  else fail "Docker Hello World"\n \
+fi' >> /home/poweruser/.bashrc
+
+### DOCKER COMPOSE ###
+RUN sudo curl -sL https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose\
+   && sudo chmod +x /usr/local/bin/docker-compose
+RUN    echo '### DOCKER ###\n \
+cyan "Docker Compose:"; docker-compose --version' >> /home/poweruser/.bashrc
+
+#RUN sudo apt-get -qq update \
+#   && sudo apt-get install build-essential dkms
+
+#https://www.vagrantup.com/docs/providers/virtualbox/boxes
+#apt-get install linux-headers-$(uname -r) build-essential dkms
+ENV DOCKER_ENV=docker
--- u18-terraform/Dockerfile.sh
+++ u18-terraform/Dockerfile.sh
@@ -1,65 +1,9 @@
-FROM ubuntu:18.04
-CMD ["/bin/bash"]
-
-RUN apt-get -qq update \
-  && apt-get -qq install \
-    sudo
-RUN echo "ALL ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers \
-  && useradd -rm -s /bin/bash -d /home/poweruser -U -G sudo -u 1001 poweruser
-USER poweruser
-WORKDIR /home/poweruser
-ENV DOCKER_ENV=terraform
-RUN   echo 'clear\n \
-if [ -d _assets/bash_history/ ]; then export HISTFILE="${HOME}/_assets/bash_history/history.${DOCKER_ENV}" && echo "Shared bash history at: ${HISTFILE}"; else echo "bash history not persisted: ${HISTFILE}"; fi\n \
-export HISTTIMEFORMAT="%F	%T	"' >> /home/poweruser/.bashrc
-
-RUN sudo apt-get -qq update
-
-### GEN EDS- yarrgh ###
-RUN sudo apt-get -qq install \
-   curl \
-   wget \
-   gnupg2 \
-   unzip \
-   vim \
-   iputils-ping
-RUN echo "12 4" | sudo apt-get -qq install software-properties-common
-RUN sudo apt-get -qq update \
-  && sudo apt-get -qq install \
-   apt-transport-https \
-   ca-certificates \
-   gnupg-agent \
-   python \
-   git \
-   python3-pip
-RUN   sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1
-RUN   sudo ln -s /usr/bin/pip3 /usr/bin/pip
-RUN   echo '\n \
-### FUNCTIONS ###\n \
-grep "DISTRIB_DESCRIPTION" /etc/lsb-release\n \
-function showcolors { for bg in `seq 0 9`; do for fg in `seq 0 9`; do echo -n "`expr $fg` `expr $bg`: " && color `expr $fg` `expr $bg` "Tyler & Corey"; echo; done; done }\n \
-alias colors=showcolors\n \
-function color  { echo -n "$(tput setaf $1;tput setab $2)${3}$(tput sgr 0) "; }\n \
-function green  { color 4 2 "${*}"; }\n \
-function yellow { color 0 3 "${*}"; }\n \
-function red    { color 9 1 "${*}"; }\n \
-function blue   { color 6 4 "${*}"; }\n \
-function cyan   { color 9 6 "${*}"; }\n \
-function grey   { color 0 7 "${*}"; }\n \
-function pass   { echo "$(green PASS: ${*})"; }\n \
-function warn   { echo "$(yellow PASS: ${*})"; }\n \
-function fail   { echo "$(red FAIL: ${*})"; }\n \
-function info   { echo "$(grey INFO: ${*})"; }\n \
-blue "python:"; python --version\n \
-blue "pip: "; pip --version' >> /home/poweruser/.bashrc
-
-
+FROM local/u18-seed
 ### DOCKER ###
 RUN sudo apt-get install -qq \
       apt-transport-https \
       ca-certificates \
       gnupg-agent \
-      software-properties-common \
    && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - \
    && sudo apt-key fingerprint 0EBFCD88 \
    && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
@@ -129,6 +73,8 @@ RUN echo '#NORUN . $f1' >> /home/poweruser/.bashrc \
 ### COMPOSE
 RUN sudo curl -sL https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose \
    && sudo chmod +x /usr/local/bin/docker-compose
+
+ENV DOCKER_ENV=terraform
 
 #RUN sudo apt-get -y -qq update \
 #   && sudo apt-get -y install build-essential dkms
