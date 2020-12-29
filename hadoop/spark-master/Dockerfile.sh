d5faf37ff84a6aea99cf09efa1c098ca4118adc9
Brandon R. India <ezgoesit247@gmail.com>
2020-12-29 10:07:01 -0600

    added spark master/worker/scala-submit

--- hadoop/spark-master/Dockerfile.sh
+++ hadoop/spark-master/Dockerfile.sh
@@ -1,3 +1,10 @@
 FROM bde2020/spark-master:3.0.1-hadoop3.2
 
 ENV PS1="\[\033[0;34m\]\h\[\033[0m\] \[\033[0;32m\]\d \T\[\033[0m\] \[\033[0;33m\]\w\[\033[0m\]\$ "
+ENV PATH="${PATH}::/spark/bin/"
+
+### START SPARK SHELL ###
+#
+# spark-shell --master spark://spark-master:7077 --total-executor-cores 6 --executor-memory 5120m
+#
+###
