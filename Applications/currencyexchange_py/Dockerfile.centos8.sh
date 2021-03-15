c0769d31f9deb1f84a2fe172d2e4babe1b2f6aa1
Brandon R. India <ezgoesit247@gmail.com>
2021-03-15 17:23:15 -0500

    port hint & PATH to ocbin

--- Applications/currencyexchange_py/Dockerfile.centos8.sh
+++ Applications/currencyexchange_py/Dockerfile.centos8.sh
@@ -4,7 +4,7 @@ FROM local/centos8-developer:systemd as developer1
 
 # build --rm --arg=UNAME=${CUSER} --arg=gituser=${CUSER} --arg=SSH_PRIVATE_KEY=${KEYNAME} --key SSH_PRIVATE_KEY_STREAM ${KEYPATH} --usertag ${CUSER} -f Dockerfile.centos8.sh Applications/currencyexchange_py
 
-# docker exec -it $(docker run --hostname centos8 -d --rm --privileged --name ${CUSER}-currencyexchange_py -v=docker_vol:/docker_vol -v=/sys/fs/cgroup:/sys/fs/cgroup:ro local/currencyexchange_py:${CUSER}) /bin/bash
+# docker exec -it $(docker run --hostname centos8 -d -p 7878:7878 --rm --privileged --name ${CUSER}-currencyexchange_py -v=docker_vol:/docker_vol -v=/sys/fs/cgroup:/sys/fs/cgroup:ro local/currencyexchange_py:${CUSER}) /bin/bash
 
 FROM developer1 as final
 ARG UNAME
@@ -17,6 +17,8 @@ RUN useradd -ms /bin/bash -d $UPATH -U $UNAME \
 alias ls=\"ls -Altr --color=auto\" \n\
 export PS1=\"\[\033[1;34m\]\u\[\033[0m\]@\[\033[1;31m\]\h:\[\033[0;37m\]\w\[\033[0m\]\$ \" \n\
 export HISTFILE=/docker_vol/history/$UNAME.bash_history \n\
+export PATH=$PATH:/docker_vol/ocbin \n\
 cd /docker_vol \n\
 "\
 >>/$UPATH/.bashrc
+EXPOSE 7878
