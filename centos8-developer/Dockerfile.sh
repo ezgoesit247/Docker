a51082c04e72ddcacf5bbc124f625baf28141ca6
Brandon R. India <ezgoesit247@gmail.com>
2021-03-15 10:01:14 -0500

    finished centos developer image

--- centos8-developer/Dockerfile.sh
+++ centos8-developer/Dockerfile.sh
@@ -1,15 +1,16 @@
-FROM local/centos-centos8 as layer1
+FROM local/centos-centos8 as root1
 
 # build --arg=gituser=${GITUSER} --arg=SSH_PRIVATE_KEY=${GITKEYNAME} --key SSH_PRIVATE_KEY_STREAM ~/.ssh/${GITKEYNAME} --arg=DH=${DOCKERHUB_USER} --arg=DP=${DOCKERHUB_PWD} -t systemd centos8-developer
+
 # docker exec -it $(docker run --hostname centos8 -d --rm --privileged --name centos8-developer -v=docker_vol:/docker_vol -v=/sys/fs/cgroup:/sys/fs/cgroup:ro local/centos8-developer:systemd) /bin/bash
 
 RUN yum -y update \
 && yum install -y wget curl
 
-FROM layer1 as layer2
+FROM root1 as root2
 RUN yum install -y git
 
-FROM layer2 as layer3
+FROM root2 as root3
 RUN yum install -y yum-utils python3 \
 && yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo \
 && yum install -y -q docker-ce docker-ce-cli containerd.io \
@@ -17,7 +18,7 @@ RUN yum install -y yum-utils python3 \
 && chmod +x /usr/local/bin/docker-compose
 
 
-FROM layer3 as layer4
+FROM root3 as root4
 RUN alternatives --set python /usr/bin/python3 \
 && yum install -y which vim
 
@@ -42,8 +43,7 @@ RUN chmod 700 /root/.ssh \
 && sed -i 's/\/Users\/***REMOVED***/'$ROOT_SAFE_PATH'/' $GIT_CONFIG \
 && chmod 600 $SSH_PRIVATE_KEY_PATH/$SSH_PRIVATE_KEY
 
-FROM layer4 as layer5
-
+FROM root4 as root5
 ENV container docker
 RUN (cd /lib/systemd/system/sysinit.target.wants/; for i in *; do [ $i == \
 systemd-tmpfiles-setup.service ] || rm -f $i; done); \
@@ -58,8 +58,7 @@ VOLUME [ "/sys/fs/cgroup" ]
 CMD ["/usr/sbin/init"]
 RUN yum clean all
 
-FROM layer5 as layer6
-
+FROM root5 as root6
 RUN echo -e "\n\
 function showcolors { for bg in \$(seq 0 9); do for fg in \$(seq 0 9); do echo -n \"\$(expr \${fg}) \$(expr \${bg}): \" && color \$(expr \${fg}) \$(expr \${bg}) \"Tyler & Corey\"; echo; done; done }\n\
 alias colors=showcolors\n\
@@ -74,7 +73,7 @@ function pass   { echo \"\$(green PASS: \${*})\"; }\n\
 function warn   { echo \"\$(yellow PASS: \${*})\"; }\n\
 function fail   { echo \"\$(red FAIL: \${*})\"; }\n\
 function info   { echo \"\$(grey INFO: \${*})\"; }\n\
-function showalias { echo ${BASH_ALIASES[*]}; }\n\
+function showalias { echo \${BASH_ALIASES[*]}; }\n\
 \n\
 function getservices { systemctl list-units --type=service; }\n\
 function getactive { systemctl list-units --type=service --state=active; }\n\
@@ -91,18 +90,22 @@ function hello_docker {\n\
 blue \"python:\" && python --version\n\
 cyan \"Docker:\" && docker --version\n\
 cyan \"Docker Compose:\" && docker-compose --version\n\
-if systemctl start docker.service\n\
-  then cyan docker.service running: && hello_docker\n\
-  else red docker.service not started:; fi\n\
 \n\
 "\
 >> /etc/bashrc
+
+RUN echo -e "\n\
+if systemctl start docker.service\n\
+  then cyan docker.service running: && hello_docker\n\
+  else red docker.service not started:; fi\n\
+"\
+>>/root/.bashrc
 
 VOLUME ["docker_vol/history"]
 WORKDIR /docker_vol
 ENV HISTFILE=/docker_vol/history/developer.bash_history
 
-FROM layer6 AS layer7
+FROM root6 AS root
 ARG DH
 ARG DP
 ENV DOCKERHUB_USER=$DH
@@ -112,4 +115,15 @@ docker login -u ${DOCKERHUB_USER} --password-stdin <<<$(echo "${DOCKERHUB_PWD}")
 "\
 >> /etc/bashrc
 
-from layer7 as final
+FROM root as developer1
+RUN yum install -y sudo
+
+FROM developer1 as final
+ARG UNAME=default_virtual
+ARG UDIR=/home
+ARG UDIRPATH=$UDIR/$UNAME
+ARG UDIR_SAFE_PATH=\\/home\\/$UNAME
+RUN useradd -ms /bin/bash -d $UDIRPATH -U $UNAME \
+&& usermod -aG docker $UNAME \
+&& echo "ALL ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers \
+&& yum clean all
