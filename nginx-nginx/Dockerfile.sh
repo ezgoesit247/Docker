a1b544dfbbc345e7b67464379be2bf294ddd9b22
EZ <ezgoesit247@gmail.com>
2021-05-23 14:45:35 -0500

    removed .sh from Dockerfile name

--- nginx-nginx/Dockerfile.sh
+++
@@ -1,8 +0,0 @@
-FROM nginx AS tmp1
-
-
-
-FROM tmp1
-COPY assets.docker/10-listen-on-ipv6-by-default.sh /docker-entrypoint.d/
-COPY assets.docker/20-envsubst-on-templates.sh /docker-entrypoint.d/
-RUN chmod u+x docker-entrypoint.d/*
